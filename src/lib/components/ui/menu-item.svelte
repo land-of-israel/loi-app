c4ae8baae2d6f17c9dec39a1bd4f42099a7bb55b
Ivana Dobcheva <ivana.dob@gmail.com>
2026-05-21 13:28:30 +0200

    change landing page layout

---
+++ src/lib/components/ui/menu-item.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    let { title, link } : {
+        title: string,
+        link: string 
+    } = $props()
+</script>
+
+
+    <a href={link} class="text-xl font-medium px-3 py-2 bg-brand-300
+     hover:bg-brand-400 hover:text-white rounded-md self-center">
+        {title}
+    </a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 import MenuCard from '$lib/components/ui/menu-card.svelte';
-import { BookOpen, Users, Search, MessageSquareText, TextWrap, Wrench } from '@lucide/svelte';
+import MenuItem from "$lib/components/ui/menu-item.svelte";
+import { Search, BookOpenText, Wrench } from '@lucide/svelte';
 import { resolve } from "$app/paths";
 </script>
 
@@ -12,35 +13,27 @@ import { resolve } from "$app/paths";
             from the rise of Islam to the First Crusade (7th–11th centuries).</p>
     </div>
     <div class="grid gap-10 md:grid-cols-3 md-gap-5 py-20 px-10">
-        <div class="grid ">
-            <MenuCard title="Browse by Passage" description="See all passages in the database, filter by work, author or keyword"
+            <MenuCard heading="Browse the database" description="See all entries in tablular format; filter by work, author or keyword"
             color="green"
-            icon={MessageSquareText} link={resolve('/passages')}/>
-        </div>
-        <div class="grid">
-            <MenuCard title="Browse by Works" description="See all works in the database, filter by author or keyword"
-            color="fuchsia"
-            icon={BookOpen} link={resolve('/works')}/>
-        </div>
-        <div class="grid">
-            <MenuCard title="Browse by Authors" description="See all authors in the database, filter by work, passage or keyword"
+            icon={BookOpenText}>
+            <div class="grid grid-cols-2 mx-auto gap-2 md:gap-4 p-5">
+                <MenuItem title="Passages" link={resolve('/passages')}/>
+                <MenuItem title="Works" link={resolve('/works')}/>
+                <MenuItem title="Authors" link={resolve('/passages')}/>
+                <MenuItem title="Keywords" link={resolve('/passages')}/>
+                </div>
+            </MenuCard>
+
+            <MenuCard description="Advanced search coming soon"
+                color="sky"
+                icon={Search}>
+                    <MenuItem title="Search" link={resolve('/search')}/>
+            </MenuCard>
+           
+           <MenuCard description="Place to find charts, graph & Co ... in the future"
             color="amber"
-            icon={Users} link={resolve('/authors')}/>
-    </div> 
-    <div class="grid">
-            <MenuCard title="Browse by Keywords" description="See all keywords in the database, filter by work, author or passage"
-            color="red"
-            icon={TextWrap} link={resolve('/keywords')}/>
-    </div> 
-     <div class="grid">
-            <MenuCard title="Search" description="Quo usque tandem catilina abutere patientia nostra"
-            color="cyan"
-            icon={Search} link={resolve('/search')}/>
-    </div> 
-     <div class="grid">
-            <MenuCard title="Explore" description="Quam diu furor ipse tuus etiam nos eludet"
-            color="pink"
-            icon={Wrench} link={resolve('/tools')}/>
-    </div> 
+            icon={Wrench}>
+                <MenuItem title="Tools" link={resolve('/tools')}/>
+            </MenuCard>
     </div>
 </div>
